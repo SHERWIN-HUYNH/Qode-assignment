7debe7e00162be7a1cdc81a8998106b45daf5b08
sherwin-huynh <huynhchitrung020503@gmail.com>
2026-03-12 22:36:01 +0700

    Fix: add postinstall into package.json

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,6 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
       
       <main className="flex-1 max-w-7xl mx-auto px-6 py-8 w-full">
         <UploadSection onUploadSuccess={handleUploadSuccess} />
